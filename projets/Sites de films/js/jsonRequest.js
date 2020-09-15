50ffe902b9041fff7f8edf139265d6f18fd29f58
abdelHoss <70784117+abdelHoss@users.noreply.github.com>
2020-09-15 17:11:22 -0400

    Abdel portfolio

--- projets/Sites de films/js/jsonRequest.js
+++ projets/Sites de films/js/jsonRequest.js
@@ -1,5 +1,5 @@
 async function getData() {
-    let url = "http://localhost/netflix/data/films.json";
+    let url = "https://abdelhoss.github.io/abdelportfolio.github.io/projets/Sites de films/data/films.json";
     let response = await fetch(url);
     response = await response.json();
     return response;
