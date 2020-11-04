bcfe27653be59bf38fa92c30cd9ce2fdcad08974
abdelHoss <70784117+abdelHoss@users.noreply.github.com>
2020-11-03 20:21:59 -0500

    Updated calculator

--- projets/Calculatrice/js/calculate.js
+++ projets/Calculatrice/js/calculate.js
@@ -1,26 +1,89 @@
 
 let theInput = document.getElementById("theResult");
-
-
-function theAdding(num) {
+let tabChar = [];
+let error;
+let symbol = document.querySelector('.symbol');
+let equation = "";
+function theAdding(char, _bool) {
+  if(!error) {
+    switch(_bool) {
+      case true:
+        theInput.value += char;
+        break;
+        default:
   
-  theInput.value += num;
+          if(theInput.value.length > 0) {
+            tabChar.push(theInput.value);
+            theInput.value = '';
+  
+            if(char == '*') {
+              symbol.innerHTML  ='&times;'
+            }
+            
+            else if(char == '/') {
+              symbol.innerHTML  ='&#xf7;'
+            }
+            else {
+              symbol.innerHTML  = char;
+            }
+            
+            if(!isNaN(tabChar[tabChar.length - 1])) {
+              tabChar.push(char);
+            }
+      
+          }
+          
+    }
+  }
+  
+
  
+}
+
+function erase() {
+  document.querySelector('#theResult').value = '';
+  tabChar = [];
+  symbol.innerHTML = '';
+  error = false;
+  equation = '';
+
 }
 
 
 function equal() {
-            
-  theInput.value = eval(theInput.value);    
+ if(!error) {
+  if(isNaN(tabChar[tabChar.length - 1]) && theInput.value == '' || tabChar.length == 0) {
+    theInput.value = 'ERROR';
+    error = true;
+    symbol.innerHTML = '';
+  }
+  else {
+    tabChar.push(theInput.value);
+  }
+  
+  for(let i of tabChar) {
+    let row = tabChar.indexOf(i); 
+    equation += i;
+    // alert(equation +" "+row);
+  }
+  // alert(equation);
 
+  theInput.value = eval(equation);  
     
+  equation = '';
+  tabChar = [];
+  symbol.innerHTML = '';  
+ }
 }
 
 
 function theReturn() {
-  theVal = theInput.value;
-    
-theInput.value = theVal.substring(0,theVal.length - 1);   
+  
+  if(!error) {
+    theVal = theInput.value;  
+  theInput.value = theVal.substring(0,theVal.length - 1);
+  tabChar.pop();
+  }
     
 }
 
