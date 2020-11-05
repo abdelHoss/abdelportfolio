a4bedfeb16754080ef0dc94eb7f1e81054fe1872
abdelHoss <70784117+abdelHoss@users.noreply.github.com>
2020-11-04 23:16:55 -0500

    Update in projects

--- projets/Tic-tac-to/js/tic.js
+++ projets/Tic-tac-to/js/tic.js
@@ -1,244 +1,240 @@
-var listBlock = [];
-var listNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-listBlock[0] = [document.getElementById('t-l-block'), false, false];
-listBlock[1] = [document.getElementById('t-m-block'), false, false];
-listBlock[2] = [document.getElementById('t-r-block'), false, false];
-listBlock[3] = [document.getElementById('m-l-block'), false, false];
-listBlock[4] = [document.getElementById('m-m-block'), false, false];
-listBlock[5] = [document.getElementById('m-r-block'), false, false];
-listBlock[6] = [document.getElementById('b-l-block'), false, false];
-listBlock[7] = [document.getElementById('b-m-block'), false, false];
-listBlock[8] = [document.getElementById('b-r-block'), false, false];
-
-var radioX = document.getElementById('radio-X');
-var radioO = document.getElementById('radio-O');
-
-var chooseX;
-var aWin;
-var pause;
-var winPlay = document.getElementById('winAudio');
-var losePlay = document.getElementById('loseAudio');
-var fairPlay = document.getElementById('fairAudio');
-
-function theWin() {
-    winPlay.play();
-    aWin = true;
-    pause = true;
-    document.getElementById('theModal').style.display = "block";
-    $('#theResult').html('You have won');
-}
-
-function theLoss() {
-    losePlay.play();
-    aWin = true;
-    pause = true;
-    document.getElementById('theModal').style.display = "block";
-    $('#theResult').html('You have lost');
-}
-
-function fairGame() {
-    fairPlay.play();
-    aWin = true;
-    pause = true;
-    document.getElementById('theModal').style.display = "block";
-    $('#theResult').html('Fair game');
-}
+let status = ['', '', '', '', '', '', '', '', ''];
+let filled = [false, false, false, false, false, false, false, false, false];
+let radioX = document.querySelector('#radio-X');
+let radioO = document.querySelector('#radio-O');
+let button = document.querySelector('.start-btn');
+let block;
+let isX;
+let image = '';
+let opp = '';
+let oppSymbol = '';
+let symbol = '';
+let pursueWrite = true;
+let appWrote = false;
+let stopGame = false;
+let count;
+let random = [];
+let randomNum;
+let blockOpp;
+let addOne;
+let arg = [];
+let args = [];
+let winPlay = document.getElementById('winAudio');
+let losePlay = document.getElementById('loseAudio');
+let fairPlay = document.getElementById('fairAudio');
+let modal = document.getElementById('theModal');
+let result = document.querySelector('#theResult');
+let barClass;
 
 function choose() {
-    if (radioX.checked || radioO.checked) {
 
-        chooseX = false;
-        if (radioX.checked) {
-            chooseX = true;
-        }
+    if (radioX.checked) {
+        isX = true;
+        image = "url('images/x-logo.svg')";
+        opp = "url('images/o-logo.svg')";
+        symbol = 'X';
+        oppSymbol = 'O';
+
+
+    } else if (radioO.checked) {
+        isX = false;
+        image = "url('images/o-logo.svg')";
+        opp = "url('images/x-logo.svg')";
+        oppSymbol = 'X';
+        symbol = 'O';
+    } else {
+        alert('Choose you symbol before starting')
+    }
+    if (isX || !isX && isX !== undefined) {
         radioX.disabled = true;
         radioO.disabled = true;
-
-    } else {
-        alert('Choose X or O');
+        button.value = 'Restart game';
+        button.addEventListener("click", function () {
+            location.reload();
+        })
     }
-}
-
-function autoDraw() {
-
-    var blockRemp = 0;
-    for (var check in listBlock) {
-        if (listBlock[check][1] === true || listBlock[check][2] === true) {
-            blockRemp++;
-        }
-    }
-
-    if (blockRemp !== listBlock.length && !aWin) {
-        do {
-            var getIndex = Math.floor(Math.random() * listNumber.length);
-        } while (listNumber[getIndex] === undefined);
-
-        var clickedBox = listBlock[getIndex][0];
-        if (chooseX) {
-            clickedBox.style.background = 'url("images/o-icon.png")';
-            listBlock[getIndex][2] = true;
-        } else {
-            clickedBox.style.background = 'url("images/x-icon.svg")';
-            listBlock[getIndex][1] = true;
-
-        }
-        delete listNumber[getIndex];
-        clickedBox.style.backgroundSize = "14em 10em";
-        clickedBox.style.backgroundRepeat = "no-repeat";
-        checkHor();
-        pause = false;
-    } 
-}
-
-function draw(x) {
-
-    // alert(pause);
-    if (chooseX !== undefined && !listBlock[x][1] && !listBlock[x][2] && !pause) {
-        //document.getElementById("winAudio").play();
-        var clickedBox = listBlock[x][0];
-        if (chooseX) {
-            clickedBox.style.background = 'url("images/x-icon.svg")';
-            listBlock[x][1] = true;
-
-        } else {
-            clickedBox.style.background = 'url("images/o-icon.png")';
-            listBlock[x][2] = true;
-
-        }
-        delete listNumber[x];
-        clickedBox.style.backgroundSize = "14em 10em";
-        clickedBox.style.backgroundRepeat = "no-repeat";
-        pause = true;
-        setTimeout(autoDraw, 1500);
-        checkHor();
-    }
-}
-
-
-function checkHor() {
-
-    if (listBlock[0][1] && listBlock[1][1] && listBlock[2][1]) {
-        if (chooseX) {
-            theWin();
-        } else {
-            theLoss();
-        }
-        aWin = true;
-    } else if (listBlock[0][2] && listBlock[1][2] && listBlock[2][2]) {
-        if (chooseX) {
-            theLoss();
-        } else {
-            theWin();
-        }
-    } else if (listBlock[3][1] && listBlock[4][1] && listBlock[5][1]) {
-        if (chooseX) {
-            theWin();
-        } else {
-            theLoss();
-        }
-    } else if (listBlock[3][2] && listBlock[4][2] && listBlock[5][2]) {
-        if (chooseX) {
-            theLoss();
-        } else {
-            theWin();
-        }
-    } else if (listBlock[6][1] && listBlock[7][1] && listBlock[8][1]) {
-        if (chooseX) {
-            theWin();
-        } else {
-            theLoss();
-        }
-    } else if (listBlock[6][2] && listBlock[7][2] && listBlock[8][2]) {
-        if (chooseX) {
-            theLoss();
-        } else {
-            theWin();
-        }
-    }
-    checkVer();
 
 }
 
 
-function checkVer() {
-    if (listBlock[0][1] && listBlock[3][1] && listBlock[6][1]) {
-        if (chooseX) {
-            theWin();
+function draw(num) {
+    block = document.getElementsByClassName('block')[num];
+    if (!filled[num] && isX !== undefined && pursueWrite && !stopGame) {
+        block.style.backgroundImage = image;
+        filled[num] = true;
+        pursueWrite = false;
+        if (isX) {
+            status[num] = 'X'
         } else {
-            theLoss();
+            status[num] = 'O'
         }
-    } else if (listBlock[0][2] && listBlock[3][2] && listBlock[6][2]) {
-        if (chooseX) {
-            theLoss();
-        } else {
-            theWin();
-        }
-    } else if (listBlock[1][1] && listBlock[4][1] && listBlock[7][1]) {
-        if (chooseX) {
-            theWin();
-        } else {
-            theLoss();
-        }
-    } else if (listBlock[1][2] && listBlock[4][2] && listBlock[7][2]) {
-        if (chooseX) {
-            theLoss();
-        } else {
-            theWin();
-        }
-    } else if (listBlock[2][1] && listBlock[5][1] && listBlock[8][1]) {
-        if (chooseX) {
-            theWin();
-        } else {
-            theLoss();
-        }
-    } else if (listBlock[2][2] && listBlock[5][2] && listBlock[8][2]) {
-        if (chooseX) {
-            theLoss();
-        } else {
-            theWin();
-        }
+
+        setTimeout(function () {
+            autoDraw(0, 1, 2);
+            autoDraw(3, 4, 5);
+            autoDraw(6, 7, 8);
+            autoDraw(0, 3, 6);
+            autoDraw(1, 4, 7);
+            autoDraw(2, 5, 8);
+            autoDraw(0, 4, 8);
+            autoDraw(2, 4, 6);
+        }, 1000);
+
     }
-    checkDiag();
-    
 }
 
-function checkDiag() {
+function autoDraw(a, b, c) {
 
-    if (listBlock[0][1] && listBlock[4][1] && listBlock[8][1]) {
-        if (chooseX) {
-            theWin();
-        } else {
-            theLoss();
+    let oppCount = 0;
+    let myCount = 0;
+
+    for (let i in arguments) {
+        if (status[arguments[i]] == oppSymbol) {
+            oppCount++;
+
+        } else if (status[arguments[i]] == symbol) {
+            myCount++;
         }
-    } else if (listBlock[0][2] && listBlock[4][2] && listBlock[8][2]) {
-        if (chooseX) {
-            theLoss();
-        } else {
-            theWin();
-        }
-    } else if (listBlock[2][1] && listBlock[4][1] && listBlock[6][1]) {
-        if (chooseX) {
-            theWin();
-        } else {
-            theLoss();
-        }
-    } else if (listBlock[2][2] && listBlock[4][2] && listBlock[6][2]) {
-        if (chooseX) {
-            theLoss();
-        } else {
-            theWin();
-        }
+
     }
-    checkTotal();
+
+    if (oppCount == 2) {
+        for (let x in arguments) {
+            if (!filled[arguments[x]] && !appWrote && !stopGame) {
+                block = document.getElementsByClassName('block')[arguments[x]];
+                filled[arguments[x]] = true;
+                status[arguments[x]] = oppSymbol;
+                block.style.backgroundImage = opp;
+                blockOpp = false;
+                appWrote = true;
+                addOne = false;
+
+            }
+        }
+    } else if (myCount == 2 && oppCount < 1) {
+        arg.push(a, b, c);
+        blockOpp = true;
+    } else if (oppCount == 1 && myCount < 1) {
+        args.push(a, b, c);
+        addOne = true;
+    }
+
+    if (a == 2 && b == 4 && c == 6) {
+        random = [];
+
+
+        if (blockOpp && !appWrote && !stopGame) {
+            arg.forEach((index) => {
+                if (!filled[index] && blockOpp) {
+                    block = document.getElementsByClassName('block')[index];
+                    block.style.backgroundImage = opp;
+                    filled[index] = true;
+                    status[index] = oppSymbol;
+                    blockOpp = false;
+                    arg = [];
+                }
+            });
+
+        } else if (addOne && !appWrote && !stopGame && !blockOpp) {
+            args.forEach((index) => {
+                if (!filled[index] && addOne) {
+                    block = document.getElementsByClassName('block')[index];
+                    block.style.backgroundImage = opp;
+                    filled[index] = true;
+                    status[index] = oppSymbol;
+                    addOne = false;
+                    args = [];
+                }
+            });
+        } else if (!blockOpp && !appWrote && !stopGame) {
+            for (y in filled) {
+                if (!filled[y]) {
+                    random.push(y)
+                }
+            }
+
+            do {
+                randomNum = random[Math.floor(Math.random() * random.length)];
+            } while (filled[randomNum]);
+            block = document.getElementsByClassName('block')[randomNum];
+            block.style.backgroundImage = opp;
+            filled[randomNum] = true;
+            status[randomNum] = oppSymbol;
+        }
+
+        pursueWrite = true;
+        appWrote = false;
+    }
+    verify(0, 1, 2);
+    verify(3, 4, 5);
+    verify(6, 7, 8);
+    verify(0, 3, 6);
+    verify(1, 4, 7);
+    verify(2, 5, 8);
+    verify(0, 4, 8);
+    verify(2, 4, 6);
 }
 
-function checkTotal() {
-     var blockRemp = 0;
-    for (var check in listBlock) {
-        if (listBlock[check][1] === true || listBlock[check][2] === true) {
-            blockRemp++;
+
+function verify(d, e, f) {
+    if (!stopGame) {
+
+        if (status[d] === 'X' && status[e] === 'X' && status[f] === 'X') {
+            switch (isX) {
+                case true:
+                    win(d, e, f);
+                    break;
+                default:
+                    lose(d, e, f);
+            }
+        } else if (status[d] === 'O' && status[e] === 'O' && status[f] === 'O') {
+            switch (isX) {
+                case true:
+                    lose(d, e, f);
+                    break;
+                default:
+                    win(d, e, f);
+            }
         }
-    } if (blockRemp == listBlock.length && !aWin) {
-        fairGame();
+
+        if (d == 2 && e == 4 && f == 6) {
+            if (!stopGame) {
+                let countFilled = 0;
+                for (z in filled) {
+                    if (filled[z]) {
+                        countFilled++;
+                    }
+                }
+                if (countFilled == 9) {
+                    fair();
+                }
+            }
+        }
     }
+}
+
+function win(d, e, f) {
+    barClass = `line_${d}_${e}_${f}`;
+    winPlay.play();
+    stopGame = true;
+    modal.style.display = "block";
+    result.innerHTML = 'You have won the game';
+    document.getElementsByClassName(barClass)[0].style.display = 'block';
+}
+
+function lose(d, e, f) {
+    barClass = `line_${d}_${e}_${f}`;
+    losePlay.play();
+    stopGame = true;
+    modal.style.display = "block";
+    result.innerHTML = 'You lost the game';
+    document.getElementsByClassName(barClass)[0].style.display = 'block';
+}
+
+
+function fair() {
+    fairPlay.play();
+    stopGame = true;
+    modal.style.display = "block";
+    result.innerHTML = 'Fair match';
 }
