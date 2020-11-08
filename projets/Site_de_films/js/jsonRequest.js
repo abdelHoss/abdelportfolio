18f3dd9df4c5ff7d10c9c5ee97110582aa93dedb
abdelHoss <70784117+abdelHoss@users.noreply.github.com>
2020-11-08 14:45:11 -0500

    Update jsonRequest.js

--- projets/Site_de_films/js/jsonRequest.js
+++ projets/Site_de_films/js/jsonRequest.js
@@ -1,5 +1,5 @@
 async function getData() {
-    let url = "https://abdelhoss.github.io/abdelportfolio/projets/Sites de films/data/films.json";
+    let url = "https://abdelhoss.github.io/abdelportfolio/projets/Site_de_films/data/films.json";
     let response = await fetch(url);
     response = await response.json();
     return response;
