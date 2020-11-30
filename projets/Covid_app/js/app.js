969d401e2b492d54715753aeba1554c7abcbff60
abdelHoss <70784117+abdelHoss@users.noreply.github.com>
2020-11-30 17:55:45 -0500

    Covid app

---
+++ projets/Covid_app/js/app.js
@@ -0,0 +1,43 @@
+
+ covidApp.controller('covidController', ['$scope', '$http', '$templateCache',
+     function ($scope, $http, $templateCache) {
+         $scope.countryTab = [];
+
+         $scope.showList = (num) => {
+            let sent = $(`.learn-p:eq(${num})`).text(); 
+            if(sent == ' Click here to learn more') {
+                $(`.learn-p:eq(${num})`).text('Reduce this');
+            }
+            else {
+                $(`.learn-p:eq(${num})`).text(' Click here to learn more');
+
+            }
+            $(`.toggle-list:eq(${num})`).slideToggle('slow');
+        
+         }
+
+         $scope.showLinks = () => {
+            $(".navbar-burger").toggleClass("is-active");
+            $(".navbar-menu").toggleClass("is-active");
+         }
+           
+         $scope.scrollTop = () => {
+            $('html, body').animate({
+                scrollTop: $('#theHeader').offset().top
+            }, 400);
+        }
+
+
+         $http({
+             method: 'GET',
+             url: 'https://corona.lmao.ninja/v2/countries?yesterday&sort',
+             cache: $templateCache
+         }).then((res) => {
+             $scope.status = res.status;
+             $scope.tabCountry = res.data;
+             $scope.count = res.data.length
+         })
+
+     
+     }
+ ]);
---
+++ projets/Covid_app/js/routes.js
@@ -0,0 +1,27 @@
+const covidApp = angular.module('covidApp', ['ngRoute','ngAnimate']);
+
+ covidApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+
+     $locationProvider.html5Mode(true);
+
+     $routeProvider
+         .when('/', {
+             templateUrl: 'views/home.html'
+         })
+         .when('/covid-news', {
+            templateUrl: 'views/feeds.html',
+            controller: 'mapController'
+         })
+         .when('/world-data', {
+            templateUrl: 'views/map.html',
+            controller: 'mapController'
+         })
+         .when('/countries', {
+             templateUrl: 'views/worldTab.html',
+             controller: 'covidController'
+         })
+         .otherwise({
+             redirectTo: '/'
+         });
+ }]);
