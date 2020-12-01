c21b3d7d97db88f457ef48de938aabd1299b9877
abdelHoss <70784117+abdelHoss@users.noreply.github.com>
2020-12-01 00:34:10 -0500

    Update routes.js

--- projets/Covid_app/js/routes.js
+++ projets/Covid_app/js/routes.js
@@ -1,22 +1,22 @@
  const covidApp = angular.module('covidApp', ['ngRoute','ngAnimate']);
  covidApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-const gitUrl = 'https://abdelhoss.github.io/abdelportfolio/projets/Covid_app';
+// const gitUrl = 'https://abdelhoss.github.io/abdelportfolio/projets/Covid_app/';
 
      $locationProvider.html5Mode(true);
 
      $routeProvider
-         .when(gitUrl+'/', {
+         .when('/', {
              templateUrl: 'views/home.html'
          })
-         .when(gitUrl+'/covid-news', {
+         .when('/covid-news', {
             templateUrl: 'views/feeds.html',
             controller: 'mapController'
          })
-         .when(gitUrl+'/world-data', {
+         .when('/world-data', {
             templateUrl: 'views/map.html',
             controller: 'mapController'
          })
-         .when(gitUrl+'/countries', {
+         .when('/countries', {
              templateUrl: 'views/worldTab.html',
              controller: 'covidController'
          })
